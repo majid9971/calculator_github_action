97764989474e7b09f0cd2cc25419becdcdc5575b
Majid Siddiqui <97821088+majid9971@users.noreply.github.com>
2025-01-07 17:32:05 +0530

    Update App.java

--- src/main/java/com/cybage/alm/App.java
+++ src/main/java/com/cybage/alm/App.java
@@ -22,69 +22,69 @@ public class App {
         return a * b;
     }
 
-    // Introduced larger duplicated code blocks
+    // // Introduced larger duplicated code blocks
 
-    // Duplicated sum method
-    public int sumCopy1(int x, int y) {
-        int result = x + y;
-        return result;
-    }
+    // // Duplicated sum method
+    // public int sumCopy1(int x, int y) {
+    //     int result = x + y;
+    //     return result;
+    // }
 
-    public int sumCopy2(int x, int y) {
-        int result = x + y;
-        return result;
-    }
+    // public int sumCopy2(int x, int y) {
+    //     int result = x + y;
+    //     return result;
+    // }
 
-    public int sumCopy3(int x, int y) {
-        int result = x + y;
-        return result;
-    }
+    // public int sumCopy3(int x, int y) {
+    //     int result = x + y;
+    //     return result;
+    // }
 
-    // Duplicated minus method
-    public int minusCopy1(int x, int y) {
-        int result = x - y; 
-        return result;
-    }
+    // // Duplicated minus method
+    // public int minusCopy1(int x, int y) {
+    //     int result = x - y; 
+    //     return result;
+    // }
 
-    public int minusCopy2(int x, int y) {
-        int result = x - y; 
-        return result;
-    }
+    // public int minusCopy2(int x, int y) {
+    //     int result = x - y; 
+    //     return result;
+    // }
 
-    public int minusCopy3(int x, int y) {
-        int result = x - y; 
-        return result;
-    }
+    // public int minusCopy3(int x, int y) {
+    //     int result = x - y; 
+    //     return result;
+    // }
 
-    // Duplicated divide method
-    public int divideCopy1(int x, int y) {
-        int result = x / y;
-        return result;
-    }
+    // // Duplicated divide method
+    // public int divideCopy1(int x, int y) {
+    //     int result = x / y;
+    //     return result;
+    // }
 
-    public int divideCopy2(int x, int y) {
-        int result = x / y;
-        return result;
-    }
+    // public int divideCopy2(int x, int y) {
+    //     int result = x / y;
+    //     return result;
+    // }
 
-    public int divideCopy3(int x, int y) {
-        int result = x / y;
-        return result;
-    }
+    // public int divideCopy3(int x, int y) {
+    //     int result = x / y;
+    //     return result;
+    // }
 
-    // Duplicated multiply method
-    public int multiplyCopy1(int x, int y) {
-        int result = x * y;
-        return result;
-    }
+    // // Duplicated multiply method
+    // public int multiplyCopy1(int x, int y) {
+    //     int result = x * y;
+    //     return result;
+    // }
 
-    public int multiplyCopy2(int x, int y) {
-        int result = x * y;
-        return result;
-    }
+    // public int multiplyCopy2(int x, int y) {
+    //     int result = x * y;
+    //     return result;
+    // }
 
-    public int multiplyCopy3(int x, int y) {
-        int result = x * y;
-        return result;
-    }
+    // public int multiplyCopy3(int x, int y) {
+    //     int result = x * y;
+    //     return result;
+    // }
 }
