2887e46f2c6f3cd197129654b085256ab6a5e185
Majid Siddiqui <97821088+majid9971@users.noreply.github.com>
2025-01-07 15:00:35 +0530

    Update App.java

--- src/main/java/com/cybage/alm/App.java
+++ src/main/java/com/cybage/alm/App.java
@@ -6,20 +6,36 @@ package com.cybage.alm;
  */
 public class App {
 
-	public int sum(int a, int b) {
-		return a + b;
-	}
+    public int sum(int a, int b) {
+        return a + b;
+    }
 
-	public int minus(int a, int b) {
-		return a - b; 
-	}
+    public int minus(int a, int b) {
+        return a - b; 
+    }
 
-	public int divide(int a, int b) {
-		return a / b;
-	}
+    public int divide(int a, int b) {
+        return a / b;
+    }
 
-	public int multiply(int a, int b) {
-		return a * b;
-	}
+    public int multiply(int a, int b) {
+        return a * b;
+    }
 
+    // Duplicated method to trigger SonarQube quality gate failure due to code duplication
+    public int sumCopy(int x, int y) {
+        return x + y;
+    }
+
+    public int minusCopy(int x, int y) {
+        return x - y; 
+    }
+
+    public int divideCopy(int x, int y) {
+        return x / y;
+    }
+
+    public int multiplyCopy(int x, int y) {
+        return x * y;
+    }
 }
